b463e7b67fab5b4ef75e7ce9dd504b3b74105b72
conkatenate <katrinaglaeser@gmail.com>
2017-09-07 22:07:08 -0700

    adding in dependencies for firebase

--- app/src/main/java/com/example/katrinaglaeser/stavaforcats/MainActivity.java
+++ app/src/main/java/com/example/katrinaglaeser/stavaforcats/MainActivity.java
@@ -9,6 +9,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override

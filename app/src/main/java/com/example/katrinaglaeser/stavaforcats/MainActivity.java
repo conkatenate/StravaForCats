b03c94954f9458bd8ea261d6051828fe4b58e603
Katrina Glaeser <katrinaglaeser@Katrina-Glaesers-iMac.local>
2017-09-14 07:12:05 -0700

    rolladding in auth

--- app/src/main/java/com/example/katrinaglaeser/stavaforcats/MainActivity.java
+++ app/src/main/java/com/example/katrinaglaeser/stavaforcats/MainActivity.java
@@ -24,10 +24,13 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
 
-
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -39,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+
+    public static final int RC_SIGN_IN = 1;
+
 
     private ListView mMessageListView;
     //private MessageAdapter mMessageAdapter;
@@ -55,12 +61,32 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mMessagesDatabaseReference;
     private ChildEventListener mChildEventListener;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUsername = ANONYMOUS;
+        // Initialize Firebase components
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        // Initialize references to views
+        //mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        //mMessageListView = (ListView) findViewById(R.id.messageListView);
+        //mPhotoPickerButton = (ImageButton) findViewById(R.id.photoPickerButton);
+        //mMessageEditText = (EditText) findViewById(R.id.messageEditText);
+        //mSendButton = (Button) findViewById(R.id.sendButton);
+
+        // Initialize progress bar
+        //mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -73,7 +99,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    /*mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                //user is signed in
+                Toast.makeText(MainActivity.this, "You are now signed in. Welcome to KatFriendlyChat!", Toast.LENGTH_SHORT).show();
+            } else {
+                //user is signed out
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setIsSmartLockEnabled(false)
+                                .setProviders(
+                                        AuthUI.EMAIL_PROVIDER,
+                                        AuthUI.GOOGLE_PROVIDER)
+                                .build(),
+                        RC_SIGN_IN);
+    */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
